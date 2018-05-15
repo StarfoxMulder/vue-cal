895bd6a33d123418730702e9fd62f02fb3600a30
StarfoxMulder <cjprestia@gmail.com>
2018-05-15 00:07:20 -0400

    basic calendar structure, about to begin mutable +/- month component

---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="classObject">{{ day.format('D') }}</div>
+</template>
+<script>
+export default {
+  props: ["day"],
+  computed: {
+    classObject() {
+      let today = this.day.isSame(this.$moment(), "day");
+      return {
+        // including the class day on all instances of CalendarDay, but conditionally adding the classes 'today' and 'past' based on their relation to the current day/month
+        day: true,
+        today,
+        past: this.day.isSameOrBefore(this.$moment(), "day") && !today
+      };
+    }
+  }
+};
+</script>
